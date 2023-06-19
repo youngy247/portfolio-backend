b1b4932ea9711086e8784e5f3d1cdcddb427c568
Adam Young <adamyoungy678@gmail.com>
2023-06-19 16:52:46 +0100

    feat: added remote db connection

--- app.js
+++ app.js
@@ -42,6 +42,8 @@ const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_A
 
 // Create a MySQL connection pool
 const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
